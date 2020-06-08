097d212eca479f67845ab717c6c19d26eb419ce8
evil-god <petermayr@ymail.com>
2020-06-08 11:15:47 +0200

    TB8504: init: disable DPM features on Wifi Device only.
    Change-Id: Id76808a8b19d071aa5e21b583ec9aeea7ae43837

--- init/init_lenovo_tb8504.cpp
+++ init/init_lenovo_tb8504.cpp
@@ -67,6 +67,7 @@ static void set_fingerprint()
 		property_override_dual("ro.product.device", "ro.vendor.product.device", "TB-8504F");
 		property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "Lenovo/TB-8504F/TB-8504F:8.1.0/OPM1.171019.019/8504F_S001011_181016_ROW:user/release-keys");
 		property_override_dual("ro.product.model", "ro.vendor.product.model", "Lenovo TB-8504F");
+		property_override("persist.vendor.dpm.feature", "0");
 	//for installing stock OTA with TWRP
 		property_override("ro.product.ota.model", "Lenovo TB-8504F");
     } else if(baseband == "msm") {
