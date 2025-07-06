eba68b8337a26182e10547e6ae8bc77470c786a8
adazem009 <info@adazem.com>
2025-07-06 14:28:22 +0000

    TB8504: camera: Add missing display include path
    hardware/qcom-caf/msm8996/display/libqdutils/qdMetaData.h:33:10: fatal error: 'color_metadata.h' file not found
    #include <color_metadata.h>
             ^~~~~~~~~~~~~~~~~~

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -117,11 +117,13 @@ endif
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
+        $(call project-path-for,qcom-display)/include
+LOCAL_C_INCLUDES += \
         $(call project-path-for,qcom-display)/libqservice
 LOCAL_C_INCLUDES += \
         $(call project-path-for,qcom-display)/libqdutils
 LOCAL_C_INCLUDES += \
-        $(call project-path-for,qcom-display)/libgralloc
+        $(call project-path-for,qcom-display)/gralloc
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqdutils libqservice libbinder
